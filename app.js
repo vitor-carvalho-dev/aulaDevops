2e5628eccea25558d56f57bd6e907331fb6477be
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-02-27 22:42:59 -0300

    atividade questao dois

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 80;
 
 const requestHandler = (request, response) => {
-  response.end('Bem-vindo ao AWS CodePipeline com Node.js! FOIIIIII!!!');
+  response.end('ATIVIDADE QUESTAO 2');
 };
 
 const server = http.createServer(requestHandler);
